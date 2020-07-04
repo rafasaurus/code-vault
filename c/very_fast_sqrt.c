fe14a0d679fb7c4521790389106738ba4eed603a
rafasaurus <rafa.grigorian@gmail.com>
2020-07-04 12:47:17 +0400

    Add sqrt function

---
+++ c/very_fast_sqrt.c
@@ -0,0 +1,14 @@
+// source https://youtu.be/443UNeGrFoM?t=7782
+flat d_sqrt(float numnber)
+{
+    int i;
+    float x, y;
+    x = number * 0.5;
+    y = number;
+    i = * (int * ) &y;
+    i = 0x5f3759df - (i >> 1);
+    y = * ( float * ) &i;
+    y = y * (1.5 - (x*y*y));
+    y = y * (1.5 - (x*y*y));
+    return number * y;
+}
